ee3be52792a951231fa89073ade11edfc634a023
Shinkei <shinkeygentatsu@yahoo.com.mx>
2018-03-11 21:02:48 -0500

    update react and use the createRef functionality to get the input value

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,23 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component{
+  myInput = React.createRef();
+
+  /* this is the way to bind the method to the object
+   * there you create an attribute that is binded by default
+   * and assign it a function, so it is not necessary to bind
+   * the function in the constructor
+   * */
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this.myInput);
+  }
+
   render(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A StorePicker</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     );
